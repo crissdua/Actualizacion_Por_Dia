34fd240ce399994e7b5c2e16601ab316507d2ea5
crissdua <crissdua@hotmail.com>
2017-04-20 09:58:50 -0600

    nueva solucion

--- Actualizacion_Por_Dia/Form1.vb
+++ Actualizacion_Por_Dia/Form1.vb
@@ -2,11 +2,19 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Button1.Enabled = False
         Button2.Enabled = False
-        'Dim objetoEscribir2 As New C4C_SaldoClientesB.CreateResponse
-        'Dim leerrequest2 As New C4C_SaldoClientesB.CreateRequest
 
-        'Dim objetoEscribir As New C4C_SaldoClientesV.CreateResponse
-        'Dim leerrequest As New C4C_SaldoClientesV.CreateRequest
+        Dim objetoActualizarC4CV As New C4C_SaldoClientesV.UpdateResponse
+        Dim clienteC4CV As New C4C_SaldoClientesV.Y0I9I95ZY_VistasClient
+        clienteC4CV.ClientCredentials.UserName.UserName = "_VIEWS"
+        clienteC4CV.ClientCredentials.UserName.Password = "Welcome08"
+        Dim leerrequestC4CV As New C4C_SaldoClientesV.UpdateRequest
+
+        Dim objetoActualizarC4CB As New C4C_SaldoClientesB.UpdateResponse
+        Dim clienteC4CB As New C4C_SaldoClientesB.Y0I9I95ZY_ClientesBClient
+        clienteC4CB.ClientCredentials.UserName.UserName = "_VIEWS"
+        clienteC4CB.ClientCredentials.UserName.Password = "Welcome08"
+        Dim leerrequestC4CB As New C4C_SaldoClientesB.UpdateRequest
+
 
         Dim request As IF_SaldoClientesA.SaldoClientesAType = New IF_SaldoClientesA.SaldoClientesAType
         Dim response As IF_SaldoClientesA.SaldoClientesAResponseType = New IF_SaldoClientesA.SaldoClientesAResponseType
@@ -22,51 +30,32 @@
         End If
         response = cliente.ZSaldoClientesA(request)
         Label1.Text = "                Cargando datos a C4C                       NO CIERRE LA APLICACION NI APAGUE EL EQUIPO"
-        'leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync = New C4C_SaldoClientesB.Saldo_ClienteBSaldoClienteBCreateRequestMessage_sync
-        'leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB = New C4C_SaldoClientesB.Saldo_ClienteBSaldoClienteBCreateRequest
-        'objetoEscribir2.Saldo_ClienteBSaldoClienteBCreateConfirmation_sync = New C4C_SaldoClientesB.Saldo_ClienteBSaldoClienteBCreateConfirmationMessage_sync
-        'leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Nombre = New C4C_SaldoClientesB.Text
-        'leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Saldo = New C4C_SaldoClientesB.Text
 
 
-        'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync = New C4C_SaldoClientesV.Saldo_ClienteVSaldoClienteVCreateRequestMessage_sync
-        'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV = New C4C_SaldoClientesV.Saldo_ClienteVSaldoClienteVCreateRequest
-        'objetoEscribir.Saldo_ClienteVSaldoClienteVCreateConfirmation_sync = New C4C_SaldoClientesV.Saldo_ClienteVSaldoClienteVCreateConfirmationMessage_sync
-        'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV.Nombre = New C4C_SaldoClientesV.Text
-        'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV.Saldo = New C4C_SaldoClientesV.Quantity
-
-
-        'Dim cliente2 As New C4C_SaldoClientesV.YGGG2714Y_Saldo_ClienteVClient
-        'cliente2.ClientCredentials.UserName.UserName = "_VIEWS"
-        'cliente2.ClientCredentials.UserName.Password = "Welcome08"
-
-        'Dim cliente3 As New C4C_SaldoClientesB.Y3YVY3FOY_SaldoClienteBClient
-        'cliente3.ClientCredentials.UserName.UserName = "_VIEWS"
-        'cliente3.ClientCredentials.UserName.Password = "Welcome08"
         Try
-
+            leerrequestC4CV.ClientesVClientesVUpdateRequest_sync = New C4C_SaldoClientesV.ClientesVClientesVUpdateRequestMessage_sync
+            leerrequestC4CV.ClientesVClientesVUpdateRequest_sync.ClientesV = New C4C_SaldoClientesV.ClientesVClientesVUpdateRequest
+            leerrequestC4CB.ClientesBClientesBUpdateRequest_sync = New C4C_SaldoClientesB.ClientesBClientesBUpdateRequestMessage_sync
+            leerrequestC4CB.ClientesBClientesBUpdateRequest_sync.ClientesB = New C4C_SaldoClientesB.ClientesBClientesBUpdateRequest
+            leerrequestC4CV.ClientesVClientesVUpdateRequest_sync.ClientesV.Saldo = New C4C_SaldoClientesV.Quantity
+            leerrequestC4CB.ClientesBClientesBUpdateRequest_sync.ClientesB.Saldo = New C4C_SaldoClientesB.Text
             For Each a As IF_SaldoClientesA.SaldoClientesAResponseTypeRow In response.SaldoClientesAResult
-                'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV.Entry = a.CardCode
-                'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV.Nombre.Value = a.CardName
-                'leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync.Saldo_ClienteV.Saldo.Value = a.Balance
-                'objetoEscribir.Saldo_ClienteVSaldoClienteVCreateConfirmation_sync = cliente2.Create(leerrequest.Saldo_ClienteVSaldoClienteVCreateRequest_sync)
-                DataGridView1.Rows.Add(New String() {a.CardCode, a.CardName, a.Balance})
-                'If a.Balance > 0 Then
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Entry = a.CardCode
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Nombre.Value = a.CardName
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Saldo.Value = "TRUE"
-                '    objetoEscribir2.Saldo_ClienteBSaldoClienteBCreateConfirmation_sync = cliente3.Create(leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync)
-                'Else
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Entry = a.CardCode
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Nombre.Value = a.CardName
-                '    leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync.Saldo_ClienteB.Saldo.Value = "FALSE"
-                '    objetoEscribir2.Saldo_ClienteBSaldoClienteBCreateConfirmation_sync = cliente3.Create(leerrequest2.Saldo_ClienteBSaldoClienteBCreateRequest_sync)
-                'End If
+                leerrequestC4CV.ClientesVClientesVUpdateRequest_sync.ClientesV.Entry = a.CardCode
+                objetoActualizarC4CV.ClientesVClientesVUpdateConfirmation_sync = New C4C_SaldoClientesV.ClientesVClientesVUpdateConfirmationMessage_sync
+                leerrequestC4CV.ClientesVClientesVUpdateRequest_sync.ClientesV.Saldo.Value = a.Balance
+                objetoActualizarC4CV.ClientesVClientesVUpdateConfirmation_sync = clienteC4CV.Update(leerrequestC4CV.ClientesVClientesVUpdateRequest_sync)
 
+                leerrequestC4CB.ClientesBClientesBUpdateRequest_sync.ClientesB.Entry = a.CardCode
+                objetoActualizarC4CB.ClientesBClientesBUpdateConfirmation_sync = New C4C_SaldoClientesB.ClientesBClientesBUpdateConfirmationMessage_sync
+                leerrequestC4CB.ClientesBClientesBUpdateRequest_sync.ClientesB.Saldo.Value = a.Balance
+                objetoActualizarC4CB.ClientesBClientesBUpdateConfirmation_sync = clienteC4CB.Update(leerrequestC4CB.ClientesBClientesBUpdateRequest_sync)
+
+
+                DataGridView1.Rows.Add(New String() {a.CardCode, a.CardName, a.Balance})
             Next
             Button1.Enabled = Enabled
             Button2.Enabled = Enabled
-            Label1.Text = "CARGA MASIVA COMPLETADA!!"
+            Label1.Text = "Actualizado!!"
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
